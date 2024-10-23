11253a4e0b68c07629feac6096f1c6d9d9a19aef
Haron <artusick@list.ru>
2024-10-23 18:51:32 +0300

    added an adaptive background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,24 @@
 </script>
 
 <template>
-  <h1>Hello world!!!</h1>
-</template>
+    <body>
+        <video autoplay muted loop>
+            <source src="../assets/video/Day.mp4" type="video/mp4">
+        </video>
+    </body>
+  </template>
 
 <style scoped>
+
+video {
+    position: absolute;
+    transform: scaleX(-1);
+    object-fit: cover;
+    object-position: 90%;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
 </style>
